a0f79106e3d72711a68fb3c5a676291941683e8f
saurav004 <logtosaurav@gmail.com>
2020-11-17 22:32:23 +0530

    Added:Welcome to Line Comparison Program

---
+++ src/main/java/LineComparison/App.java
@@ -0,0 +1,10 @@
+package LineComparison;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program" );
+    }
+}
